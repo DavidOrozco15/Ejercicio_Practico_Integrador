101637e332fc2b909e396b5d4c72c978050c14db
jorgegmch <gomezjorge.ch@gmail.com>
2025-10-15 20:29:08 -0500

    feat: Se agregó funcionalidad para calcular perimetro de un polígono

---
+++ superficies/poligono.py
@@ -0,0 +1,10 @@
+# from utils import clear_screen
+
+def perimetro_poligono(n, lados):
+    # clear_screen()
+    print("CALCULAR EL PERIMETRO DE UN POLÍGONO REGULAR (P = n x L)")
+    print("-" * 50)
+    n = int(input("\nIngrese el número de lados que tiene el polígono: "))
+    lados = float("Ingrese el valor de uno de los lados del polígono: ")
+    per_poligono_regular = n * lados
+    print(f"El perimetro del polígono es igual a {per_poligono_regular}")
