f9a2fc3fe08fe5669254815f7f2449f12a522bf0
DavidOrozco15 <juanodavid94@gmail.com>
2025-10-15 19:01:13 -0500

    se hizo el menu de analisis numerico

---
+++ analisisNumerico/__init__.py
@@ -0,0 +1,23 @@
+def menuAnalisis():
+    while True:
+        print("---ANALISIS NUMERICO---")
+        print("\n1. Factorial")
+        print("2. Fibonacci")
+        print("3. Numeros Amigos")
+        print("4. Numeros Perfectos")
+        print("0. Volver")
+        
+        opcion = int(input("\nIngrese una opcion: "))
+        match opcion:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 0:
+                pass
+            case _:
+                pass
